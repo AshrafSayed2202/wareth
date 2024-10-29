06827fc57884e7d08f4f716fbabad695658e3f2f
ashraf9932 <ashraf.neizk@gmail.com>
2024-10-29 07:41:11 +0300

    merge blogs styles

--- src/components/BlogsPage.jsx
+++ src/components/BlogsPage.jsx
@@ -1,21 +1,22 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import BlogsLanding from '../assets/images/blogs-landing.jpg'
+import styles from '../assets/styles/blogs.module.css';
 const BlogsPage = () => {
     React.useEffect(() => {
         document.title = 'الوارث | المدونات'
     }, [])
     return (
         <section>
-            <div className="page-header">
+            <div className={styles.pageHeader}>
                 <div className="container">
                     <p><Link to={'/'}>الصفحة الرئيسية</Link>{' > '}<span>المدونات</span></p>
-                    <p className="head">المدونات</p>
+                    <p className={styles.head}>المدونات</p>
                 </div>
             </div>
-            <img src={BlogsLanding} alt='blogs landing' className='blog-landing' />
+            <img src={BlogsLanding} alt='blogs landing' className={styles.blogLanding} />
             <div className="container">
-                <h1 className='blog-header'>المدونات</h1>
+                <h1 className={styles.blogHeader}>المدونات</h1>
             </div>
         </section>
     )
