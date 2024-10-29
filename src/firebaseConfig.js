16aa99c892d9bdcd52b0efc266fa272b635e5f81
ashraf9932 <ashraf.neizk@gmail.com>
2024-10-29 18:46:18 +0300

    .env added

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDjPwhSpYl74p6QK16re45WjgohSApRGC0",
-    authDomain: "el-wareth.firebaseapp.com",
-    projectId: "el-wareth",
-    storageBucket: "el-wareth.appspot.com",
-    messagingSenderId: "231590554318",
-    appId: "1:231590554318:web:f3f71a1ad48460fe67fe9f",
-    measurementId: "G-8DS97KTEWJ"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
