512620d4f7604ffc930325d19067cd2559a1ae9f
ashraf9932 <ashraf.neizk@gmail.com>
2024-11-01 20:46:59 +0200

    Login Styles added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,11 +3,13 @@ import React, { useState, useEffect } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebaseConfig";
 import { useNavigate } from "react-router-dom";
+import styles from '../assets/styles/Login.module.css'; // Import the CSS module
 
 const Login = () => {
     useEffect(() => {
-        document.title = 'الوارث | تسجيل دخول '
-    }, [])
+        document.title = 'الوارث | تسجيل دخول';
+    }, []);
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
@@ -23,22 +25,24 @@ const Login = () => {
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleLogin}>
+        <div className={styles.container}>
+            <h2 className='heading'>تسجيل دخول</h2>
+            <form className={styles.form} onSubmit={handleLogin}>
                 <input
                     type="email"
-                    placeholder="Email"
+                    placeholder="البريد الإلكتروني"
+                    className={styles.input} // Apply input styles
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
                 <input
                     type="password"
-                    placeholder="Password"
+                    placeholder="كلمة المرور"
+                    className={styles.input} // Apply input styles
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
-                <button type="submit">Login</button>
+                <button type="submit" className={styles.button}>تسجيل دخول</button> {/* Apply button styles */}
             </form>
         </div>
     );
